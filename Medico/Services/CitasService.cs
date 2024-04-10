48578daf3fcd9ebd7ce0052e0239fa8419eb768b
Omar Alejandro Rodríguez Márquez <oa.rodriguez@ugto.mx>
2024-04-09 19:27:47 -0600

    CitasPacientes

---
+++ Medico/Services/CitasService.cs
@@ -0,0 +1,16 @@
+﻿using Medico.Data;
+using Medico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medico.Services
+{
+    public class CitasService (ApplicationDbContext context) : ICitas
+    {
+        public async Task<List<Cita>> ObtenerCitasHoy()
+        {
+            List<Cita> citas = new List<Cita>();
+            citas = await context.Citas.ToListAsync();
+            return citas;
+        }
+    }
+}
